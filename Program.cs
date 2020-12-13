82b71854054f72744220d8180ede387fb289441c
tomeczkoo <75810121+tomeczkoo@users.noreply.github.com>
2020-12-13 01:06:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+namespace Fajguji
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			Kwadrat k1 = new Kwadrat(4.2); 
+			Console.WriteLine(k1.obliczPole());
+			Console.WriteLine(k1.obliczObwod());
+			
+			Kolo kol1 = new Kolo(8.1);
+			Console.WriteLine(kol1.obliczPole());
+			Console.WriteLine(kol1.obliczObwod());
+			
+			
+		
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
